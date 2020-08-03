23285271c5211327189e1c8ad951e1370ab1ca48
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-08-03 14:55:23 -0700

    Updates

---
+++ precache-manifest.7da10eca200bb98bf3a6cee223855497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5189767c08761faa9af577f7aff734a",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e54c58d377087f766da7",
+    "url": "/random-quote-machine/static/css/main.6a436b71.chunk.css"
+  },
+  {
+    "revision": "b83353c9ac56e52ee95c",
+    "url": "/random-quote-machine/static/js/2.f352a338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.f352a338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54c58d377087f766da7",
+    "url": "/random-quote-machine/static/js/main.3077c971.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
